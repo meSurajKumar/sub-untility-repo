7fa1894f9ca8aeb8cd7885b35132822c4976e72e
meSurajKumar <mesuraj751@gmail.com>
2026-02-04 22:49:04 +0530

    add auto steam handling

--- file-system/buffer/handling-largefiles.js
+++ file-system/buffer/handling-largefiles.js
@@ -27,48 +27,38 @@ const copyWithProgress = (fileName) => {
                 copiedSize += chunk.length;
                 const percentage = Math.round((copiedSize / totalSize) * 100);
                 process.stdout.write(`\r Progress : ${percentage} [${copiedSize} / ${totalSize} bytes]`)
-                // Critical : Handle Back-Pressuse;
-                if (!writeStream.write(chunk)) {
-                    // write buffer is full, pause reading;
-                    readStream.pause();
-                }
             });
 
-            // Handle Back-Pressure resume reading when write buffer is drained.
-            writeStream.on('drain', () => {
-                readStream.resume();
-            });
-
-            // 3.5 Event , End when the read is complete.
-            readStream.on('end', () => {
-                writeStream.end(); // single writestream to finish
-            });
-
-            // 4. EVENT : 'end' (Jab read kahtam ho jaye)
-            // this code is good and can also work but if the error occurs in the readstreams then they will not reach to the try-catch block here th
-            // readStream.on('end', () => {
-            //     console.log(`\n Reading Fineshed!`)
-            // })
-
-            // Improvised code ... 4.
-            writeStream.on('finish', () => {
-                process.stdout.write('\n');
-                console.log(`Copy Complte : ${fileName}`);
-                resolve();
-            });
-
+            //  -- 2. ERROR HANDLING (Cleanup)...
+            const cleanup = () => {
+                readStream.destroy();
+                writeStream.end(); // also can use destroy() here.
+            };
             // 5. EVENT : 'Error' (agar kuch fat jaye) // improving the error handig
             const handleError = (err) => {
                 console.log(`\n Error : ${err.message}`);
-                readStream.destroy(); // cleanup;
-                writeStream.destroy();
+                cleanup();
                 reject(err)
             };
             readStream.on('error', handleError);
             writeStream.on('error', handleError);
 
+            writeStream.on('finish', () => {
+                process.stdout.write('\n');
+                console.log(`Copy Complte : ${fileName}`);
+                resolve();
+            });
+            readStream.pipe(writeStream);
+
+            // --- 5. CRITICAL: Handle Ctrl+C (Process Termination) ---
+            process.on('SIGINT', () => {
+                console.log('\n\n Process Cancelled by User. Cleaning up...');
+                readStream.destroy();
+                writeStream.destroy();
+                process.exit();
+            })
+
             // 6. Magic Line : Pipe connect 
-            // readStream.pipe(writeStream);
         } catch (error) {
             reject(error);
         };
